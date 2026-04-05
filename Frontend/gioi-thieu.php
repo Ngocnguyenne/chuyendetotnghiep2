e569a773bcfb8a019468a0ef9a4770f17deb1a6b
Ngọc <127615894+Ngocnguyenne@users.noreply.github.com>
2026-04-05 11:10:00 +0700

    Create gioi-thieu.php

---
+++ Frontend/gioi-thieu.php
@@ -0,0 +1,6 @@
+<?php include "include/header.php"; ?>
+<div class="container" style="background: white; padding: 50px; min-height: 400px;">
+    <h1>Giới Thiệu Về Web Phim</h1>
+    <p>Chào mừng bạn đến với website xem phim trực tuyến của 11 .</p>
+</div>
+<?php include "include/footer.php"; ?>
